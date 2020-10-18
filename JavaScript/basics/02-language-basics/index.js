7edc0569ae5b36ec3caab3ed96f1d1903622e956
sfl-jakeprim <sufulu@foxmail.com>
2020-10-18 17:16:10 +0800

    nt

---
+++ JavaScript/basics/02-language-basics/index.js
@@ -0,0 +1,9 @@
+// const person1 = {};
+// person1.name = '123';
+// console.log(person1);
+// person1 = {};//指向一个新的引用地址 Assignment to constant variable.
+
+let i =0;
+for (const j = 7;i<5;i++) {
+    console.log(j);
+}
